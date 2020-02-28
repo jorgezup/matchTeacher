51029ff99a8685e902701f31cd6d605458815bc3
Jorge Zupirolli <jorgebertolo@gmail.com>
2020-02-28 10:50:51 -0400

    Primeiro commit: adicionado pagina index dos professores, e pagina create com o formulario de criacao

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require("./routes")
+
+const app = express()
+
+app.use(express.static('public')) /* cria uma pasta para arquivos públicos */
+app.use(routes)
+
+app.set("view engine", "njk") /* motor de busca dos arquivos */
+
+/* configurações do nunjucks */
+nunjucks.configure("views", {
+    express: app,
+    autoescape: false,
+    noCache: true
+})
+
+
+/* ================================== */
+/* Inicia o servidor na porta 5000 */
+app.listen(5000, ()=> {
+    console.log('server is running')
+})
